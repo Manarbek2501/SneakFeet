5d2ea155d6105534821f62986bbc24e8eacb6acf
Manarbek.2501 <b.manarbek@icloud.com>
2023-05-22 16:34:04 +0600

    Initial Commit

---
+++ SneakFeet/SneakFeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SneakFeetApp.swift
+//  SneakFeet
+//
+//  Created by Manarbek Bibit on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SneakFeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
