e27693e27d054f2c5ad4f6aca4a553a1a6c42cf5
karaevablay <karaevablay@gmail.com>
2023-07-01 22:46:17 +0600

    checking best work among the studs

--- SneakFeet/SneakFeetApp.swift
+++ SneakFeet/SneakFeetApp.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import FirebaseCore
 
+
+// Krasapet
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
